93e8e12746d2e208cc0b4f4523a112ffaaa2fd03
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2020-04-19 20:24:01 -0700

    Updates to store.js code
    Sorting of items in JSON file

--- js/store.js
+++ js/store.js
@@ -39,6 +39,51 @@ dataRequest.onload = function e() {
 		showMore[0].style.display = "none";
 	}
 	
+	function getKey(index) {
+		if (index < data.length) {
+			return data[index].key;
+		}
+		return -1;
+	}
+	
+	function pushToArray() {
+		var list = new Array();
+		var sorted = new Array();
+		
+		//Push keys to array
+		var i = 0;
+		for (i; i < data.length; i++) {
+			list.push(i);
+		}
+		
+		var pos = 0;
+		//selection sort
+		for (var i = 0; i < list.length; i++) {
+			var current = data[list[i]].price;
+			var highestVal = 0;
+			var highestValLoc;
+			console.log("List is now: " + list);
+			for (var j = i; j < list.length; j++) {
+				var nextItem = data[list[j]].price;
+				if (i != j) {
+					console.log(current + " - " + nextItem);
+					if (current < nextItem && highestVal < nextItem) {
+						//swap
+						var temp = list[j];
+						list[j] = list[i];
+						list[i] = temp;
+						highestVal = nextItem;
+						highestValLoc = j;
+					}
+				}
+			}
+			console.log("----HIGHEST IS: " + highestVal);
+		}
+		console.log("length: " + list.length);
+		console.log(list);
+		return list;
+	}
+	
 	var i = 0;
 	function defaultView() {
 		if (resetStatus == 1) {
@@ -49,15 +94,15 @@ dataRequest.onload = function e() {
 		if (getItemCount() > 0) {
 			if (getItemCount() > ITEMS_PER_PAGE) {
 				displayShowMore();
+			}
 				while (i+1 <= getItemCount() && i+1 <= pageLocation*ITEMS_PER_PAGE) {
 					writeHere[0].innerHTML += "<div class=\'items\'>"
 			 		 + "<img src=\'" + data[i].src + "\'/>"
 					 + "<div class=\'item-name\'><hr><b><font size=\'5\'>" + data[i].name + "</font></b></div>"
-					 + "<div class=\'item-price-container\'><div class=\'item-price\'>" + data[i].price + "</div></div>"
+					 + "<div class=\'item-price-container\'><div class=\'item-price\'> $" + data[i].price + "</div></div>"
 					 + "</div>";
 					i++
 				}
-			}
 		}
 		showMore[0].onclick = function() {
 			pageLocation += 1;
@@ -65,35 +110,6 @@ dataRequest.onload = function e() {
 			if (i+1 > itemCount) {
 				hideShowMore();
 			}
-		}
-	}
-	
-	var highestItem = 0;
-	var highestLoc = 0;
-	var currentItem = 0;
-	var currentLoc = 0;
-	var temp;
-	function highestView() {
-		clearView();
-		if (resetStatus == 1) {
-			resetStatus = 0;
-			i = 0;
-			pageLocation = 1;
-		}
-		while (i+1 < getItemCount()) {
-			if (data[i+1].price > data[i].price) {
-				highestLoc = i+1;
-			}
-			i++;
-		}
-		i = 0;
-		while (i+1 <= getItemCount() && i+1 <= pageLocation*ITEMS_PER_PAGE) {
-			writeHere[0].innerHTML += "<div class=\'items\'>"
-			 		 + "<img src=\'" + data[highestLoc].src + "\'/>"
-					 + "<div class=\'item-name\'><hr><b><font size=\'5\'>" + data[highestLoc].name + "</font></b></div>"
-					 + "<div class=\'item-price-container\'><div class=\'item-price\'>" + data[highestLoc].price + "</div></div>"
-					 + "</div>";
-					i++
 		}
 	}
 	
@@ -111,14 +127,16 @@ dataRequest.onload = function e() {
 	}
 	
 	function main() {
+		var list = pushToArray();
+		for (var i = 0; i < list.length; i++) {
+			console.log(data[list[i]].price);
+		}
 		defaultView();
-	
 		reset[0].onclick = function() {
 			resetView();
 		}
 		
 		highest.onclick = function() {
-			highestView();
 		}
 	}
 	main();
