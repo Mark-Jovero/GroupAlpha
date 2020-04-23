4c653f17939bcb30850628f82e34cf7376315d8f
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2020-04-23 13:50:49 -0700

    Update typeFilter.js

--- js/typeFilter.js
+++ js/typeFilter.js
@@ -1,7 +1,6 @@
 var array = [];
 var tracker = 0;
 function loadTypes() {
-	console.log("Loading item types...");
 	for (var i = 0; i < data.length; i++) {
 		var fixedCase = data[i].type;
 		fixedCase = fixedCase.substring(0,1).toUpperCase() + data[i].type.substring(1, data[i].type.length);
@@ -14,29 +13,24 @@ function loadTypes() {
 		}
 	}
 	array.sort();
-	console.log("Item types loaded.");
 }
 
 function resetView() {
 		resetStatus = 1;
 		writeHere[0].innerHTML = "";
-		console.log("resetView() called");
 		defaultView();
 }
 	
 function clearView() {
 		resetStatus = 1;
 		writeHere[0].innerHTML = "";
-		console.log("clearView() called");
 }
 
 var writeCat = document.getElementsByClassName("item_type");
 function writeCategories() {
-	console.log("Writing item types to div...");
 	for (tracker; tracker < array.length; tracker++) {
 		writeCat[0].innerHTML += "<div class=\'category\'>" + array[tracker] + "</div>";
 	}
-	console.log("Item types written.");
 }	
 
 function main() {
@@ -47,10 +41,7 @@ function main() {
 	writeCategories();
 }
 
-document.onload = function() {
-	main();
-}
-
-window.onload = function() {
-	main();
-}
+document.addEventListener('mousemove', function() {
+   main();
+   console.log("!!");
+}, false);
