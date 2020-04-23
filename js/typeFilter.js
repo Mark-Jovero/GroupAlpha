56c0fcf7b57e65034b30ebec0c3cbeec6e0b89d3
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2020-04-23 12:42:43 -0700

    store fixes

--- js/typeFilter.js
+++ js/typeFilter.js
@@ -2,12 +2,14 @@ var array = [];
 function loadTypes() {
 	console.log("Loading item types...");
 	for (var i = 0; i < data.length; i++) {
+		var fixedCase = data[i].type;
+		fixedCase = fixedCase.substring(0,1).toUpperCase() + data[i].type.substring(1, data[i].type.length);
 		if (array.length > 0) { // not empty
-			if (array.indexOf(data[i].type) == -1) { // if array does not contain category
-				array.push(data[i].type);
+			if (array.indexOf(fixedCase) == -1) { // if array does not contain category
+				array.push(fixedCase);
 			}
 		} else {
-			array.push(data[i].type);
+			array.push(fixedCase);
 		}
 	}
 	array.sort();
