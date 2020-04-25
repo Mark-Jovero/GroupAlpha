72ad1278d42b325b6857390731d8da2ba5492c6d
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2020-04-25 03:05:28 -0700

    cart functionality update

---
+++ js/addToCart.js
@@ -0,0 +1,48 @@
+var totalCartItems = 0;
+var totalCartPrice = 0;
+var cartData = [];
+window.onmousemove = function() {
+	var itemAdded = document.getElementsByClassName("itemAdded");
+	var buyFormButton = document.getElementsByClassName("item-price");
+	var cartItemCounter = document.getElementsByClassName("item_count");
+	var addToCart = document.getElementsByClassName("cart_table");
+	var subtotal = document.getElementsByClassName("subtotal");
+	var item_display_overlay = document.getElementById("popupCover");
+	
+
+	
+	buyFormButton[0].onmouseout = function() {
+		if (buyFormButton[0].innerText == "Add To Cart") {
+			if (buyFormButton[0].style.cursor == "not-allowed") {
+				buyFormButton[0].style.color = "";
+			} else {
+				buyFormButton[0].style.color = "white";
+				buyFormButton[0].style.backgroundColor = "#8288a1";
+				
+			}
+		}
+	}
+	
+	buyFormButton[0].onclick = function() {
+		nameValue = parseInt(nameValue);
+		if (buyFormButton[0].style.cursor != "not-allowed" && nameValue > 0) {
+		
+				addToCart[0].innerHTML += "<tr><td class=\'cart_image\'><img src=\'" + storeData[clickedExport].src + "\' class=\'cart_img\'></td><td class=\'item_name\'>"
+					 + storeData[clickedExport].name + "</td>" + 
+					 "<td>$" + storeData[clickedExport].price + "</td><td>" + " x (" + nameValue + ")</td>"+ "</tr>";
+			
+			totalCartPrice += storeData[clickedExport].price * nameValue;
+			totalCartItems += nameValue;
+			cartData.push(clickedExport);
+			var trimmedInt = totalCartPrice.toFixed(2);
+			subtotal[0].innerHTML = "Subtotal: $<b>" + trimmedInt + "</b>";
+			cartItemCounter[0].innerHTML =  totalCartItems;
+			itemAdded[0].style.zIndex = "900000";
+			item_display_overlay.style.zIndex = "890000";
+			setTimeout(function(){
+				itemAdded[0].style.zIndex = "-10";	
+			}, 1500);			
+
+		}
+	}
+}
