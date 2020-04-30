de42f629bc87c215af0ecbeaf3a5f6ee368d7533
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2020-04-29 20:33:27 -0700

    Store updates
    -Added items
    -Fixed scroll issues

--- products/storeData.js
+++ products/storeData.js
@@ -19,7 +19,6 @@ var storeData = [
 	"desc"		:	"A pencil you can write with!",
 	"price" 	: 	0.59,
 	"src"		:	"items/pencil.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	0
 	},
 	{
@@ -28,7 +27,6 @@ var storeData = [
 	"desc"		:	"A whiteboard. 11 by 8 inches.",
 	"price" 	: 	10.99,
 	"src"		:	"items/11_8_board.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	1
 	},
 	{
@@ -37,7 +35,6 @@ var storeData = [
 	"desc"		:	"A big whiteboard. 48 by 36 inches.",
 	"price" 	: 	24.99,
 	"src"		:	"items/48_board.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	2
 	},
 	{
@@ -46,7 +43,6 @@ var storeData = [
 	"desc"		:	"A pencil you can write with!",
 	"price" 	: 	8.99,
 	"src"		:	"items/markers.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	3
 	},
 	{
@@ -55,7 +51,6 @@ var storeData = [
 	"desc"		:	"White chalk for wrtiting on blackboards or on the sidewalk. Item is dust-less.",
 	"price" 	: 	0.99,
 	"src"		:	"items/chalk.png",
-	"page"		:	"productPage/chalk.html",
 	"keyID"		:	4
 	},
 	{
@@ -64,7 +59,6 @@ var storeData = [
 	"desc"		:	"Colorful set of chalk!",
 	"price" 	: 	4.99,
 	"src"		:	"items/assorted_chalk.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	5
 	},
 	{
@@ -73,7 +67,6 @@ var storeData = [
 	"desc"		:	"Long-lasting ball pen with comfortable grip.",
 	"price" 	: 	8.99,
 	"src"		:	"items/ball_pen_10.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	6
 	},
 	{
@@ -82,7 +75,6 @@ var storeData = [
 	"desc"		:	"All black ball point pen.",
 	"price" 	: 	2.99,
 	"src"		:	"items/ball_pen.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	7
 	},
 	{
@@ -91,7 +83,6 @@ var storeData = [
 	"desc"		:	"A single college-ruled notebook. 70 pages.",
 	"price" 	: 	0.49,
 	"src"		:	"items/collegenotebook.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	8
 	},
 	{
@@ -100,7 +91,6 @@ var storeData = [
 	"desc"		:	"96 Piece set with many different shades of colors!",
 	"price" 	: 	6.99,
 	"src"		:	"items/crayons_96.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	9
 	},
 	{
@@ -109,7 +99,6 @@ var storeData = [
 	"desc"		:	"Cheap markers! You can loose them and not worry because they are cheap!",
 	"price" 	: 	3.99,
 	"src"		:	"items/dry_erase_24.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	10
 	},
 	{
@@ -118,7 +107,6 @@ var storeData = [
 	"desc"		:	"A premium, long-lasting black gel pen.",
 	"price" 	: 	3.99,
 	"src"		:	"items/gel_pen.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	11
 	},
 	{
@@ -127,7 +115,6 @@ var storeData = [
 	"desc"		:	"A long-lasting lead 0.7 pencil. Comes with replacement erasers and lead refills.",
 	"price" 	: 	4.29,
 	"src"		:	"items/mech_pencil_2.png",
-	"page"		:	"productPage/mech_pencil_2.html",
 	"keyID"		:	12
 	},
 	{
@@ -136,7 +123,6 @@ var storeData = [
 	"desc"		:	"Low quality, cheap pencils.",
 	"price" 	: 	3.99,
 	"src"		:	"items/mech_pencil_10.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	13
 	},
 	{
@@ -145,7 +131,6 @@ var storeData = [
 	"desc"		:	"A set of pencils. Suitable for children. Pre-sharpened!",
 	"price" 	: 	3.99,
 	"src"		:	"items/premium_pencil_6.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	14
 	},
 	{
@@ -154,7 +139,6 @@ var storeData = [
 	"desc"		:	"Colorful set of classical sharpies!",
 	"price" 	: 	12.99,
 	"src"		:	"items/sharpie_assorted.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	15
 	},
 	{
@@ -163,7 +147,6 @@ var storeData = [
 	"desc"		:	"Wide-ruled notebook. 70 pages.",
 	"price" 	: 	0.49,
 	"src"		:	"items/widenotebook.png",
-	"page"		:	"productPage/pencil.html",
 	"keyID"		:	16
 	},
 	{
@@ -171,8 +154,7 @@ var storeData = [
 	"type" 		: 	["office", "furniture"],
 	"desc"		:	"A wooden desk for students!",
 	"price" 	: 	89.99,
-	"src"		:	"items/empty.png",
-	"page"		:	"productPage/pencil.html",
+	"src"		:	"items/desk.jpg",
 	"keyID"		:	17
 	},
 	{
@@ -180,9 +162,48 @@ var storeData = [
 	"type" 		: 	["office", "school"],
 	"desc"		:	"A neon highlighter",
 	"price" 	: 	1.99,
-	"src"		:	"items/empty.png",
-	"page"		:	"productPage/pencil.html",
+	"src"		:	"items/highlighter.png",
 	"keyID"		:	18
+	},
+	{
+	"name" 		: 	"Genovo X4",
+	"type" 		: 	["Computers", "school", "office"],
+	"desc"		:	"A versatile laptop! Great for gaming and school."+
+						"<br><b>Specs:</b><ul>"+
+						"<li>Intel Core i5</li>" +
+						"<li>512GB SSD / 1TB HDD</li>" +
+						"<li>15GB MEM</li>" +
+						"<li>Windows Office 365 Installed!</li>" +
+						"<li>2-year Warranty</li>" +
+						"<li>Free Carrying Case</li>" +
+						"</ul>",
+	"price" 	: 	799.99,
+	"src"		:	"items/laptop_13_in_cheap.png",
+	"keyID"		:	19
+	},
+	{
+	"name" 		: 	"3 Inch Binder",
+	"type" 		: 	["school", "office"],
+	"desc"		:	"A 3-inch binder, 3-ring. Strong hinges. Can hold up 200 pages of paper.",
+	"price" 	: 	3.99,
+	"src"		:	"items/binder.png",
+	"keyID"		:	20
+	},
+	{
+	"name" 		: 	"Apple iPad Mini 2",
+	"type" 		: 	"tablets",
+	"desc"		:	"Apple iPad Mini 2. Gray. 256GB",
+	"price" 	: 	189.99,
+	"src"		:	"items/ipadmini2.png",
+	"keyID"		:	21
+	},
+	{
+	"name" 		: 	"Samsung Galaxy Tab A 10 Inch",
+	"type" 		: 	"tablets",
+	"desc"		:	"A 10 inch tablet from Samsung! 128GB",
+	"price" 	: 	149.99,
+	"src"		:	"items/samsungtabA10.png",
+	"keyID"		:	22
 	}
 	
 ]
