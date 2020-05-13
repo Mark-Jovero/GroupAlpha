33ccc049b8a441f5c1a5eae5eda4907fc43c3b76
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2020-05-12 18:53:47 -0700

    Now checks to make sure email does not exist in Database.

--- php/createAccount.php
+++ php/createAccount.php
@@ -3,15 +3,36 @@
 	$lastName = $_POST['lastName'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
+	$link = mysqli_connect('localhost', 'connection', 'student', 'NewLeaf');
 
 $conn = new mysqli('localhost', 'connection', 'student', 'NewLeaf');
 if ($conn->connect_error) {
-	die('!sd!' .$conn->connect_error);
+	die('Error: ' .$conn->connect_error);
 } else {
-	$sts = $conn->prepare("Insert into users(firstName, lastName, email, password) VALUES(?,?,?,?)");
-	$sts->bind_param("ssss", $firstName, $lastName, $email, $password);
-	$sts->execute();
-	$sts->close();
-	$conn->close();
+	
+	//Check if email exists in database
+	$emailExists = FALSE;
+	$sql = "SELECT email FROM users";
+	if ($result = $conn -> query($sql)) {
+ 		while ($row = $result -> fetch_row()) {
+   			//printf ("%s\n", $row[0]);
+			if ($email == $row[0]) {
+				$emailExists = TRUE;	
+			}
+  		}
+  	$result -> free_result();
+	}
+	
+	if ($emailExists == FALSE) {
+		$sts = $conn->prepare("Insert into users(firstName, lastName, email, 				password) VALUES(?,?,?,?)");
+		$sts->bind_param("ssss", $firstName, $lastName, $email, $password);
+		$sts->execute();
+		$sts->close();
+		$conn->close();
+	} else {
+		header("Location: ../index.html");
+		exit;
+	}
 }
+
 ?>
