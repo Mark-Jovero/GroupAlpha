0587f0eeb8478b58f867c8d0c29507466c3a10ae
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2020-05-12 22:12:18 -0700

    Add files via upload

--- php/createAccount.php
+++ php/createAccount.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
 	if ($result = $conn -> query($sql)) {
  		while ($row = $result -> fetch_row()) {
    			//printf ("%s\n", $row[0]);
-			if ($email == $row[0]) {
+			if (strtolower($email) == strtolower($row[0])) {
 				$emailExists = TRUE;	
 			}
   		}
@@ -26,18 +26,27 @@ if ($conn->connect_error) {
 	if ($emailExists == FALSE) {
 		$sts = $conn->prepare("Insert into users(firstName, lastName, email, 				password) VALUES(?,?,?,?)");
 		$sts->bind_param("ssss", $firstName, $lastName, $email, $password);
+		$pagecontents = file_get_contents("phpResultPages/AccountCreated.html");
+		$addTo = "<h2>Hello, ". $firstName;
+		$addTo .= "</h2><h3>Your account was successfully created!</h3>";
+		$addTo .= "Email used: " . $email;
+		$addTo .= "<br>Check your email to confirm your account.";
+		$addTo .= " <br>You can now <a href=\'../../forms/login.html>log in!</a>";
+		$pagecontents = str_replace("WriteHere001", $addTo , $pagecontents);
+		echo $pagecontents;
 		$sts->execute();
 		$sts->close();
 		$conn->close();
 	} else {
-		$pagecontents = file_get_contents("../index.html");
-		$findString = "href=\"";
-		$replaceWith = "href=\"../";
-		$pagecontents2 = str_replace($findString, $replaceWith, $pagecontents);
-		$findString = "src=\"";
-		$replaceWith = "src=\"../";
-		$pagecontents2 = str_replace($findString, $replaceWith, $pagecontents);
-		echo $pagecontents2;
+		$pagecontents = file_get_contents("phpResultPages/AccountCreated.html");
+		$pagecontents = str_replace("<title>Account Created!</title>", "<title>Unable to Create Account</title>", $pagecontents);
+
+
+		$addTo = "<h2>There was a problem creating your account.</h2> The email you've entered (";
+		$addTo .= $email;
+		$addTo .= ") is already in use. <br>Please try again or contact support to reset your password.";
+		$pagecontents = str_replace("WriteHere001", $addTo , $pagecontents);
+		echo $pagecontents;
 		exit;
 	}
 }
